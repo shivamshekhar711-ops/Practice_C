c54700033f691a68879f0f7d5e877c8618824353
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2025-12-19 21:56:16 +0530

    update

---
+++ updation.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+
+    int x;
+    x=2;
+    printf("%d" ,x);
+
+    printf("\n");
+
+    x=x+2;
+    printf("%d",x);
+
+
+    x=x-1;
+    printf("\n%d",x);
+
+    
+
+
+
+
+
+}
---
+++ variable
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int x;
+
+    x=100;
+    printf("%d\n\n",x);
+
+    x=200;
+    printf("%d",x);
+
+return 0;
+}
