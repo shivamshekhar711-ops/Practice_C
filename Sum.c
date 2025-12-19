c5e67a7659b2315289e17c0754863473bd50028c
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2025-12-19 20:58:39 +0530

    update

---
+++ Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int a,b,sum;
+    printf("enter a number:");
+    scanf("%d" ,&a);
+
+    printf("enter b number:");
+
+    scanf("%d" ,&b);
+
+    sum=a+b;
+
+    printf("%d" ,sum);
+
+    return 0;
+
+}
