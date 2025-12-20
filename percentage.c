55c9e8de64fd989410fffad9adedd08469d0e370
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2025-12-20 19:01:00 +0530

    New

---
+++ percentage.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    float M ,P,C,E,H,p;
+
+     M=55;                    
+     P=85;            
+     C=89;            
+     H=99;           
+     E=68;           
+    
+     P=(M+P+C+H+E)/5;
+
+     printf("Percentage of 5 subject=> %f\n",P);
+
+     return 0;
+
+    
+}
