ba0efd7d125b0672e75e74bdb5e5d25bb8b76e88
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2025-12-24 10:30:50 +0530

    new

---
+++ nameemail.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Shivam shekhar\nshivamshekhar117@gmail.com");
+
+    return 0;
+}
---
+++ simple.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  int x, y, z;
+  x = y = z = 50;
+  printf("%d", x + y + z);
+  return 0;
+}
