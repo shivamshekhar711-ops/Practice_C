3f50a19d189bbe669c0bcd39a9d9539fb989bd2e
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2025-12-25 13:01:53 +0530

    new

---
+++ if_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int amin(){
+
+    int a;
+     printf("enter any numbner");
+     scanf("%d",a);
+
+     if(a%2==0){
+        printf("number is even %d" ,a);
+     }
+
+     else{
+        printf("numberr is odd %d" ,a);
+     }
+
+     return 0;
+    }
