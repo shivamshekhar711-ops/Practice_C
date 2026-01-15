6080ba5de1d36af6f44e19c2a98b2c8d6c19b5eb
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-01-15 19:37:36 +0530

    update

--- sumofnevennumber.c
+++ sumofnevennumber.c
@@ -5,12 +5,19 @@ int main (){
 
     printf("Enter a no");
     scanf("%d" ,&n);
+ if(n%2==0)
+ {
 
-    for(i=1; i<=n; i++){
+     for(i=1; i<=n; i++){
 
         sum=sum+i*2;
     }
 printf("%d" ,sum);
+ }
+ else
+ {
+    printf("\n number odd");
+ }
 
 return 0;
 
